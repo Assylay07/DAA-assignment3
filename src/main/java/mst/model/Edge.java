b17db1c6c1b212b37c1ffb53a949d4aed16c7a5d
Assylay <assylayshukirbay@mail.ru>
2025-10-25 17:27:58 +0500

    Added Edge class

---
+++ src/main/java/mst/model/Edge.java
@@ -0,0 +1,12 @@
+package mst.model;
+
+public class Edge {
+    public int u, v;
+    public long w;
+
+    public Edge(int u, int v, long w) {
+        this.u = u;
+        this.v = v;
+        this.w = w;
+    }
+}
