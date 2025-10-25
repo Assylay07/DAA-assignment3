3f703a1f4ed9aac8ff3bc4b463ad290dbd000983
Assylay <assylayshukirbay@mail.ru>
2025-10-25 17:29:16 +0500

    added algoResults

---
+++ src/main/java/mst/model/AlgoResult.java
@@ -0,0 +1,10 @@
+package mst.model;
+
+import java.util.List;
+
+public class AlgoResult {
+    public List<JsonEdge> mst_edges;
+    public long total_cost;
+    public long operations_count;
+    public double execution_time_ms;
+}
